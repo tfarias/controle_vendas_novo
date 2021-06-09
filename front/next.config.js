fb5111fc3599a3b86e04f54a24cd651e5a308831
Tiago <tfariasg3@gmail.com>
2021-06-09 19:03:38 -0400

    feat: update project

--- front/next.config.js
+++ front/next.config.js
@@ -4,4 +4,10 @@ module.exports = {
     API_HOST: 'http://127.0.0.1:8000/api',
   },
 
+  serverRuntimeConfig: {
+    apiUrl: 'http://tray_server/api'
+  },
+  publicRuntimeConfig: {
+    apiUrl: 'http://localhost:8000/api'
+  }
 }
--- front/src/services/api-host.ts
+++ front/src/services/api-host.ts
@@ -1,7 +1,9 @@
 import axios from 'axios';
-
+import getConfig from "next/config";
+const { serverRuntimeConfig, publicRuntimeConfig } = getConfig();
+const apiUrl = serverRuntimeConfig.apiUrl || publicRuntimeConfig.apiUrl;
 const apiHost = axios.create({
-  baseURL: 'http://127.0.0.1:8000/api',
+  baseURL: apiUrl,
 });
 
 export default apiHost;
