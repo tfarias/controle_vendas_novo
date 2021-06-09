8e79dcad969b235c9e7ed2ef23b6fb9cf585abb9
Tiago <tfariasg3@gmail.com>
2021-06-09 17:03:13 -0400

    first commit

--- front/next.config.js
+++ front/next.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   env: {
     API_URL: 'http://tray_server/api',
-    API_HOST: 'http://tray_server/api',
+    API_HOST: 'http://127.0.0.1:8000/api',
   },
 
 }
--- front/src/services/api-host.ts
+++ front/src/services/api-host.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const apiHost = axios.create({
-  baseURL: process.env.API_HOST,
+  baseURL: 'http://127.0.0.1:8000/api',
 });
 
 export default apiHost;
